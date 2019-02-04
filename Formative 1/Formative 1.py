080effc71e4a1a62d980337b9ea7dcd6aec8a91b
DAMaas <phoenix@dions-xps-13.wifi.hu.nl>
2019-02-04 12:38:54 +0100

    Fuck me

--- Formative 1/Formative 1.py
+++ Formative 1/Formative 1.py
@@ -1,2 +1,10 @@
-print("Hallo ik ben een dikke slang")
-print("Hallo ik ben een dikke slang")
+import pymongo
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017/')
+db = client['shopping']
+collection = db['products']
+
+# Tests
+
+print("Collections:")
+print(db.list_collection_names())
